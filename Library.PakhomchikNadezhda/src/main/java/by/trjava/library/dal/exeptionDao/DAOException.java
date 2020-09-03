4db82e6b9e9fbbb97849eb3f4561e67f0c4fdc42
nadi-ka <nadi_ka@mail.ru>
2020-09-03 12:54:30 +0300

    first commit

---
+++ Library.PakhomchikNadezhda/src/main/java/by/trjava/library/dal/exeptionDao/DAOException.java
@@ -0,0 +1,18 @@
+package by.trjava.library.dal.exeptionDao;
+
+public class DAOException extends Exception {
+
+    public DAOException() {};
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+}
